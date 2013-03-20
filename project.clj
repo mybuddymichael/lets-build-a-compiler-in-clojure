6cc81226b1d67792fbb273e92ab4fed89e87ecad
Michael Hanson <michael.b.hanson@gmail.com>
2013-03-19 21:43:27 -0700

    Change the license to AGPL

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "Exercises from Jack Crenshaw's Let's Build A Compiler,
                 written in Clojure."
   :url "https://github.com/mybuddymichael/lets-build-a-compiler-in-clojure"
-  :license {:name "GNU LGPL"
-            :url "http://www.gnu.org/licenses/lgpl-3.0.html"}
+  :license {:name "GNU AGLP"
+            :url "http://www.gnu.org/licenses/agpl-3.0.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
