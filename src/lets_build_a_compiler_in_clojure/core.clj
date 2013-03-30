9821e4349f42ea46a4452b5e1f12774ad502d6ed
Michael Hanson <michael.b.hanson@gmail.com>
2013-03-30 15:05:09 -0700

    Remove the core ns files

--- src/lets_build_a_compiler_in_clojure/core.clj
+++
@@ -1,6 +0,0 @@
-(ns lets-build-a-compiler-in-clojure.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
--- test/lets_build_a_compiler_in_clojure/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns lets-build-a-compiler-in-clojure.core-test
-  (:use clojure.test
-        lets-build-a-compiler-in-clojure.core))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
