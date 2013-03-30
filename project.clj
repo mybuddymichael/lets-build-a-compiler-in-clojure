6ff1cb6fce5a9ce405376cc2afea981cae8ee11d
Michael Hanson <michael.b.hanson@gmail.com>
2013-03-29 22:35:50 -0700

    Fix a typo in the license name

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "Exercises from Jack Crenshaw's Let's Build A Compiler,
                 written in Clojure."
   :url "https://github.com/mybuddymichael/lets-build-a-compiler-in-clojure"
-  :license {:name "GNU AGLP"
+  :license {:name "GNU AGPLv3"
             :url "http://www.gnu.org/licenses/agpl-3.0.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
