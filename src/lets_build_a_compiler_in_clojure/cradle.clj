6963b38e561f40c14b1ebc14c418bab644a87945
Michael Hanson <michael.b.hanson@gmail.com>
2013-04-06 11:43:02 -0700

    Revert "Remove everything for now"
    This reverts commit 0ed25406163872fd6bd3656517727f112a342505.

--- src/lets_build_a_compiler_in_clojure/cradle.clj
+++ src/lets_build_a_compiler_in_clojure/cradle.clj
@@ -13,3 +13,55 @@
 (defn expected [s]
   (throw (Exception. (str s " expected"))))
 
+(defn is-alpha? [c]
+  (re-seq #"[A-Za-z]" (str c)))
+
+(defn is-digit? [c]
+  (re-seq #"[0-9]" (str c)))
+
+(defn get-name [c]
+  (if (is-alpha? c)
+    c
+    (expected "Name")))
+
+(defn get-number [c]
+  (if (is-digit? c)
+    c
+    (expected "Integer")))
+
+(defn emitln [s]
+  (str \tab s \newline))
+
+(defn factor [s]
+  (emitln (str "MOVE #" (get-number s) ",D0")))
+
+(defn add [c]
+  (str (emitln "ADD (SP)+,D0")))
+
+(defn subtract [c]
+  (str (emitln "SUB (SP)+,D0")
+       (emitln "NEG D0")))
+
+(defn multiply [c]
+  (str (emitln "MULS (SP)+,D0")))
+
+(defn divide [c]
+  (str (emitln "MOVE (SP)+,D1")
+       (emitln "DIVS D1,D0")))
+
+
+(defn sub-expression [s]
+  (let [[op t & more] s]
+    (when op
+      (str (emitln "MOVE D0,-(SP)")
+           (factor t)
+           (cond
+             (= op \*) (str (multiply t) (sub-expression more))
+             (= op \/) (str (divide t) (sub-expression more))
+             (= op \+) (str (sub-expression more) (add t))
+             (= op \-) (str (sub-expression more) (subtract t))
+             :else (expected "Mathop"))))))
+
+(defn expression [s]
+  (str (factor (first s))
+       (sub-expression (rest s))))
